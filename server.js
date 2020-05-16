c215640cc57832281cb4153fb0ff0379ee8bd73f
mauraparker02 <mauraparker02@gmail.com>
2020-05-16 18:07:51 -0400

    getting everything set up, and initiated, restructuring and organizing

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+// const User = require("./user.js");
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true });
+
+app.get("/api/workouts", (req, res) => {
+  // User.create(body)
+  //   .then(dbUser => {
+  //     res.json(dbUser);
+  //   })
+  //   .catch(err => {
+  //     res.json(err);
+  //   });
+  console.log("reached api workouts")
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
